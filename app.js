7186a6e8c3ec9382ed18f4cc0264a591146427bf
Sharon He <sharonhe2000@outlook.com>
2020-05-16 09:12:15 -0700

    Created post, left at 38:31

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-kill me
+const express = require("express");
+const app = express();
+const port = 3000;
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://User:123321@node-queue-gtbnz.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    useMongoClient: true,
+  }
+);
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
