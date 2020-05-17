102ff1775ce22f5d7741a5664b955d6a03ecf0b2
Johnny <johnnyliao06@gmail.com>
2020-05-16 18:10:58 -0700

    implemented google maps API

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from "react";
+import Home from "./screens/Home.js";
+import { LoadScript } from "@react-google-maps/api";
+import { GoogleMap } from "@react-google-maps/api";
+import { Marker } from "@react-google-maps/api";
 
-import Home from './screens/Home.js'
-
-function App() {
+function App(props) {
+  const onLoad = (marker) => {
+    console.log("marker", marker);
+  };
+  const position = {
+    lat: 49.260368,
+    lng: -123.003769,
+  };
+  const { containerStyle, center } = props;
   return (
-		<Home />
-	);
+    <div>
+      <LoadScript googleMapsApiKey="AIzaSyCiFAWaUhuyFLDoprTBhbChbtWPPISVZBQ">
+        <GoogleMap
+          id="circle-example"
+          mapContainerStyle={{
+            height: "100vh",
+            width: "100%",
+          }}
+          zoom={15}
+          center={{
+            lat: 49.260368,
+            lng: -123.003769,
+          }}
+        >
+          <Marker onLoad={onLoad} position={position} />
+        </GoogleMap>
+      </LoadScript>
+    </div>
+  );
 }
 
 export default App;
