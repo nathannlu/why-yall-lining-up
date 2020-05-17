b3ed45b7db08a4b3dad01ffc8faa3a873bb1cd1b
nathannlu <nathanlu_@hotmail.com>
2020-05-16 22:43:15 -0700

    bruh

--- app.js
+++ app.js
@@ -24,14 +24,11 @@ mongoose.connect(process.env.DB_CONNECTION, {
 
 // Script testing
 // notifyUsers();
-
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
+app.listen(port, () => console.log(`Server up and running on port ${port}.`));
