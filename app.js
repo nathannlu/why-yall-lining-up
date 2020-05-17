8637a28dca00013511ac5d275e630aef2a1098e6
nathannlu <nathanlu_@hotmail.com>
2020-05-16 23:04:24 -0700

    penis

--- app.js
+++ app.js
@@ -24,11 +24,9 @@ mongoose.connect(process.env.DB_CONNECTION, {
 
 // Script testing
 // notifyUsers();
-if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-}
 
 app.listen(port, () => console.log(`Server up and running on port ${port}.`));
